4e6c32c73f29cb99251ca51e5aa5b770bd480f66
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2021-12-09 14:37:37 +0600

    header sticky added

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 function Header(props) {
     return (
-        <header>
+        <header className='sticky-top'>
             <Navbar></Navbar>
         </header>
     );
