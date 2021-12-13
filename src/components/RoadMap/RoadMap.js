13c765cbe8b6f63ecd9846ddb27a4ba8d617b0bd
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2021-12-13 10:00:45 +0600

    roadmap updated

--- src/components/RoadMap/RoadMap.js
+++ src/components/RoadMap/RoadMap.js
@@ -1,6 +1,72 @@
 import React from "react";
 import DesignSectionDot from "../DesignSectionDot/DesignSectionDot";
 function RoadMap(props) {
+  let roadMapObj = [
+    {
+      id: 1,
+      name: "4 MONTHS",
+      list: ["Get advice", "Customer Development", "Messaging and Positioning"],
+    },
+    {
+      id: 2,
+      name: "3 MONTHS",
+      list: [
+        "Create launch plan",
+        "Prep Marketing Team",
+        "Develop go to market strategy",
+      ],
+    },
+    {
+      id: 3,
+      name: "2 MONTHS",
+      list: [
+        "Beta test",
+        "Finalize launch content",
+        "Gather reviews from beta testers",
+      ],
+    },
+    {
+      id: 4,
+      name: "1 MONTHS",
+      list: ["Announce the launch"],
+    },
+    {
+      id: 5,
+      name: "3 WEEKS",
+      list: ["Check in with affiliats", "Start sending social media"],
+    },
+    {
+      id: 6,
+      name: "2 WEEKS",
+      list: ["Set up shoping cart", "Research pad traffic"],
+    },
+    {
+      id: 7,
+      name: "1 WEEKS",
+      list: ["Set up projct storefront", "Check all your links!"],
+    },
+    {
+      id: 8,
+      name: "LAUNCH WEEK",
+      list: [
+        "Make your launch a special event",
+        "Keep sharing on social media & email list",
+      ],
+    },
+    {
+      id: 9,
+      name: "AFTER LAUNCH",
+      list: [
+        "Celebrate!",
+        "Follow up testimonials",
+        "Take feedback and improve product",
+      ],
+    },
+  ];
+  let roadMapObjOdd = roadMapObj.filter((n) => n.id % 2 != 0);
+  //console.log(roadMapObjOdd);
+  let roadMapObjEven = roadMapObj.filter((n) => n.id % 2 == 0);
+  //console.log(roadMapObjEven)
   return (
     <div id="RoadMap" className="py-5">
       <div className="container-xxl py-5">
@@ -17,341 +83,115 @@ function RoadMap(props) {
             </h3>
           </div>
           <div className="col-10 col-sm-8 col-lg-6 text-center">
-            <p data-aos-once="true" data-aos="fade-up" data-aos-delay="400">
-              The Developer: “@Crypto_Realtor” Curtis is A Licensed Realtor, US
-              Navy Veteran & Investor
-            </p>
+            <h3 data-aos-once="true" data-aos="fade-up" data-aos-delay="400">
+              <i className="fa fa-minus" aria-hidden="true"></i> 2023{" "}
+              <i className="fa fa-minus" aria-hidden="true"></i>
+            </h3>
           </div>
         </div>
       </div>
       <div className="d-none d-xl-block" id="roadMap_line_wrapper">
         {/* top line cards */}
         <div className="top_line_cards">
-          {/* line card 1 */}
-          <div className="line_cards">
-            <div className="line_with_icons">
-              <div className="icon_wrapper">
-                <img
-                  src={require("../../assets/images/pen.svg").default}
-                  alt=""
-                />
-              </div>
-              <div className="line_content_wrapper">
-                <p>Emergence and design of the idea</p>
-              </div>
-            </div>
-            <div className="line_duration">
-              <p>Q2 2022</p>
-            </div>
-          </div>
-          {/* line card 1 End*/}
-
-          {/* line card 2 */}
-          <div className="line_cards">
-            <div className="line_with_icons">
-              <div className="icon_wrapper">
-                <img
-                  src={require("../../assets/images/cog.svg").default}
-                  alt=""
-                />
-              </div>
-              <div className="line_content_wrapper">
-                <ul className="list-unstyled text-start line_article_list">
-                  <li>
-                    <p>
-                      start development of the ealier version and crypto wallet
-                      version
+          {roadMapObjOdd.map((v) => {
+            return (
+              <div className="line_cards" key={v.id}>
+                <div className="line_with_icons">
+                  <div className="icon_wrapper">
+                    <img
+                      src={require("../../assets/images/pen.svg").default}
+                      alt=""
+                    />
+                  </div>
+                  <div className="line_content_wrapper">
+                    <ul className="list-unstyled text-start line_article_list">
+                      {v.list.map((j) => {
+                        return (
+                          <li
+                            key={j}
+                            data-aos-once="true"
+                            data-aos="fade-left"
+                            data-aos-delay={10 * v.id}
+                          >
+                            <p>{j}</p>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                  <div className="line_duration">
+                    <p
+                      data-aos-once="true"
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                    >
+                      {" "}
+                      {v.name}{" "}
                     </p>
-                  </li>
-                  <li>
-                    <p>negotiations on partnerships</p>
-                  </li>
-                </ul>
+                  </div>
+                </div>
               </div>
-              <div className="line_duration">
-                <p>Q4 2022</p>
-              </div>
-            </div>
-          </div>
-          {/* line card 2 End */}
-
-          {/* line card 3 */}
-          <div className="line_cards">
-            <div className="line_with_icons">
-              <div className="icon_wrapper">
-                <img
-                  src={require("../../assets/images/pen.svg").default}
-                  alt=""
-                />
-              </div>
-              <div className="line_content_wrapper">
-                <ul className="list-unstyled text-start line_article_list">
-                  <li>
-                    <p>Crowdsale</p>
-                  </li>
-                  <li>
-                    <p>marketing campaign</p>
-                  </li>
-                  <li>
-                    <p>listing to the major exchanges</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="line_duration">
-                <p>Q2 2022</p>
-              </div>
-            </div>
-          </div>
-          {/* line card 3 End */}
-          {/* line card 4 */}
-          <div className="line_cards">
-            <div className="line_with_icons">
-              <div className="icon_wrapper">
-                <img
-                  src={require("../../assets/images/pen.svg").default}
-                  alt=""
-                />
-              </div>
-              <div className="line_content_wrapper">
-                <ul className="list-unstyled text-start line_article_list">
-                  <li>
-                    <p>Mobile App</p>
-                  </li>
-                  <li>
-                    <p>Explorer</p>
-                  </li>
-                  <li>
-                    <p>new partnerships and new marketplace suppor</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="line_duration">
-                <p>Q4 2022</p>
-              </div>
-            </div>
-          </div>
-          {/* line card 4 End */}
-
-          {/* line card 5 */}
-          <div className="line_cards">
-            <div className="line_with_icons">
-              <div className="icon_wrapper">
-                <img
-                  src={require("../../assets/images/pen.svg").default}
-                  alt=""
-                />
-              </div>
-              <div className="line_content_wrapper">
-                <ul className="list-unstyled text-start line_article_list">
-                  <li>
-                    <p> Platform integration to electronic marketplaces</p>
-                  </li>
-                  <li>
-                    <p>Zero Fee</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="line_duration">
-                <p>Q2 2022</p>
-              </div>
-            </div>
-          </div>
-          {/* line card 5 End */}
+            );
+          })}
         </div>
       </div>
-      <div className="d-xl-none" id="roadMap_mobile_line">
+      <div
+        className="d-xl-none"
+        id="roadMap_mobile_line"
+        data-aos-once="true"
+        data-aos="fade-right"
+      >
         <span id="line_draw"></span>
         {/* left side */}
         <div className="line_content_wrapper_left">
-          <div className="line_content">
-            <h4>3 MONTHS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>{" "}
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-          <div className="line_content">
-            <h4>1 MONTHS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>{" "}
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-
-          <div className="line_content">
-            <h4>2 WEEKS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>{" "}
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-          <div className="line_content">
-            <h4>LAUNCH WEEK</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>{" "}
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
+          {roadMapObjEven.map((v) => {
+            return (
+              <div className="line_content" key={v.id}>
+                <h4> {v.name} </h4>
+                <ul className="list-unstyled m-0">
+                  {v.list.map((j) => {
+                    return (
+                      <li key={j}>
+                        {" "}
+                        <i
+                          className="fa fa-check-circle"
+                          aria-hidden="true"
+                        ></i>{" "}
+                        <p>{j}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
         </div>
         {/* left side */}
 
         <div className="line_content_wrapper_right">
           {/* right side  */}
-          <div className="line_content">
-            <h4>4 MONTHS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>{" "}
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-          <div className="line_content">
-            <h4>2 MONTHS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
 
-          <div className="line_content">
-            <h4>3 WEEKS</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-
-          <div className="line_content">
-            <h4>1 WEEK</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
-
-          <div className="line_content">
-            <h4>After Launch</h4>
-            <ul className="list-unstyled m-0">
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-              <li>
-                {" "}
-                <i className="fa fa-check-circle" aria-hidden="true"></i>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </li>
-            </ul>
-          </div>
+          {roadMapObjOdd.map((v) => {
+            return (
+              <div className="line_content" key={v.id}>
+                <h4>{v.name}</h4>
+                <ul className="list-unstyled m-0">
+                  {v.list.map((j) => {
+                    return (
+                      <li key={j}>
+                        {" "}
+                        <i
+                          className="fa fa-check-circle"
+                          aria-hidden="true"
+                        ></i>{" "}
+                        <p>{j}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
 
           {/* right side  */}
         </div>
